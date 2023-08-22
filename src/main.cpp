75b67adb85cceadd7d292b2a1e8448e4dc3b7f7a
Metehan Gezer <irrelevantdev@outlook.com>
2023-08-23 02:42:51 +0300

    styling

--- src/main.cpp
+++ src/main.cpp
@@ -11,28 +11,28 @@ int main() {
 
 
   //mcs -target:library -out:assembly.dll HelloWorldClass.cs
-  MonoAssembly *assembly = mono_domain_assembly_open(domain, "assembly.dll");
+  MonoAssembly* assembly = mono_domain_assembly_open(domain, "assembly.dll");
   if (!assembly) {
     std::cout << "Assembly not found!" << std::endl;
     return 1;
   }
 
-  const char *entryPointName = "HelloWorldNamespace.HelloWorldClass:GetHelloWorld";
-  MonoImage *image = mono_assembly_get_image(assembly);
-  MonoMethodDesc *desc = mono_method_desc_new(entryPointName, false);
-  MonoMethod *method = mono_method_desc_search_in_image(desc, image);
+  const char* entry_name = "HelloWorldNamespace.HelloWorldClass:GetHelloWorld";
+  MonoImage* image = mono_assembly_get_image(assembly);
+  MonoMethodDesc* desc = mono_method_desc_new(entry_name, false);
+  MonoMethod* method = mono_method_desc_search_in_image(desc, image);
 
   if (!method) {
     std::cout << "Method not found!" << std::endl;
     return 1;
   }
 
-  MonoObject *result = mono_runtime_invoke(method, NULL, NULL, NULL);
+  MonoObject *result = mono_runtime_invoke(method, nullptr, nullptr, nullptr);
   if (result) {
-    MonoString *resultString = mono_object_to_string(result, NULL);
-    const char *str = mono_string_to_utf8(resultString);
+    MonoString* resultString = mono_object_to_string(result, nullptr);
+    const char* str = mono_string_to_utf8(resultString);
     std::cout << "Returned string: " << str << std::endl;
-    mono_free((void *)str);
+    mono_free((void*) str);
   }
 
   mono_jit_cleanup(domain);
